3184e6f0f7974617a52967bcc7b66f981738a80c
ext.sahil.patil <ext.sahil.patil@elastic.run>
2022-11-24 18:56:16 +0530

    Stack Implementation

---
+++ stack/stack.go
@@ -0,0 +1,26 @@
+package stack
+
+type Stack []string
+
+func (s *Stack) Push(v string) {
+	*s = append(*s, v)
+}
+
+func (s *Stack) Pop() string {
+	l := len(*s)
+	if l > 0 {
+		op := (*s)[l-1]
+		*s = (*s)[:l-1]
+		return op
+	}
+	return ""
+}
+
+func (s *Stack) Top() string {
+	n := len(*s) - 1
+	return (*s)[n]
+}
+
+func (s Stack) Len() int {
+	return len(s)
+}
